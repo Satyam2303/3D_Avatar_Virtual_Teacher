9b37edaf6a40a45949eb35e1755bc977f5dfb8d1
satyam2303 <satyammunot2303@gmail.com>
2026-02-22 20:18:14 -0500

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/3D_Avatar_Virtual_Teacher/", 
 });
