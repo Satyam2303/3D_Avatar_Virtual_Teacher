d8bc1dbff6e950bdfeaa9dfdda2e5389257edd9e
satyam2303 <satyammunot2303@gmail.com>
2026-02-22 20:39:44 -0500

    deployment changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// 1. IMPORT PDFJS
+import { pdfjs } from 'react-pdf';
+
+// 2. SET THE WORKER SOURCE (Important for GitHub Pages)
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
+
 // react-pdf CSS (Text Layer + Annotations)
 import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
